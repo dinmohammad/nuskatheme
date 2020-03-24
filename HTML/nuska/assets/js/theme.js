ed7d7e5af89af041e1568544956e15014eb1c676
Din Mohammad <din.aitvet@gmail.com>
2020-03-24 21:40:15 +0600

    first commit in nuska theme

---
+++ HTML/nuska/assets/js/theme.js
@@ -0,0 +1,103 @@
+// -----------------------------
+
+//   js index
+/* =================== */
+/*  
+    
+
+    
+
+*/
+// -----------------------------
+
+
+(function($) {
+    "use strict";
+
+
+
+    /*---------------------
+    preloader
+    --------------------- */
+
+    $(window).on('load', function() {
+        $('#preloader').fadeOut('slow', function() { $(this).remove(); });
+    });
+
+
+    /*----------------------------
+     jQuery MeanMenu
+    ------------------------------ */
+    $('nav#dropdown').meanmenu();
+
+    /*-----------------
+    meanmenu 
+    -----------------*/
+    $('nav#mobile_menu_active').meanmenu({
+        meanScreenWidth: "991",
+        meanMenuContainer: '.mobile-menu-area .container',
+    });
+
+    /*-----------------
+    sticky
+    -----------------*/
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 85) {
+            $('header').addClass('navbar-fixed-top');
+        } else {
+            $('header').removeClass('navbar-fixed-top');
+        }
+    });
+
+    /*-----------------
+    scroll-up
+    -----------------*/
+    $.scrollUp({
+        scrollText: '<i class="fa fa-arrow-up" aria-hidden="true"></i>',
+        easingType: 'linear',
+        scrollSpeed: 1500,
+        animation: 'fade'
+    });
+
+    /*------------------------------
+         counter
+    ------------------------------ */
+    $('.counter-up').counterUp();
+
+
+    /*---------------------
+    testimonial slider
+    --------------------- */
+    function our_team_carousel() {
+        var owl = $(".our_team_carousel");
+        owl.owlCarousel({
+            loop: true,
+            margin: 20,
+            responsiveClass: true,
+            navigation: true,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            nav: true,
+            items: 5,
+            smartSpeed: 2000,
+            dots: true,
+            autoplay: false,
+            autoplayTimeout: 4000,
+            center: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 1
+                },
+                760: {
+                    items: 1
+                }
+            }
+        });
+    }
+    our_team_carousel();
+
+
+
+}(jQuery));
